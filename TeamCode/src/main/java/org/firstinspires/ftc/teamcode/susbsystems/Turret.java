71c16bf298ac4c25dec740e96eb14880ada4b603
fgportman <fgportman00@gmail.com>
2025-07-14 17:01:56 +0000

    Update Turret.java
    Added vector math to and todos to Turret class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susbsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susbsystems/Turret.java
@@ -10,8 +10,19 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.util.Util;
 
+//TODO 7/14/25 - Frank
+// - confirm default 0 degree position and rotation direction
+//    - affects GoTo()
+// - detect if a given point is too close to the robot
+// - integrate with extension system for a getTargetVector() function
+// - latestDirection vector is a clunky implementation. I should streamline it
+//    - current solution: add a static method to Vector2 to create a new polar vector
+// - move getVectorTarget() to the full system class, as it needs arm and extension information
+
+
 public class Turret {
-    private final double TICKS_TO_DEG = 5900d/90d;
+    //changed this variable from TICKS_TO_DEG to DEG_TO_TICKS for accuracy
+    private final double DEG_TO_TICKS = 5900d/90d;
 
     private DcMotorEx motor;
     private DigitalChannel homeLimitSwitch;
@@ -20,7 +31,11 @@ public class Turret {
     private double homePower = 0.3;
     private ElapsedTime homeTime = new ElapsedTime();
     private double targetPosition;
+    private double targetPosition
 
+    //represents the direction the turret has last been set to
+    public Vector2 latestDirection = new Vector2();
+    
     private PIDController controller;
 
     public boolean AUTOSTOP = true;
@@ -36,10 +51,27 @@ public class Turret {
         isHoming = true;
         homeTime.reset();
     }
-    public void GoTo(double targetPosition) {
-        this.targetPosition = targetPosition*TICKS_TO_DEG;
+    public void GoTo(Vector3 targetVector) {
+        GoTo(getTargetAngleDeg(targetVector));
+    }
+    
+    public void GoTo(double targetAngle) {
+        this.targetPosition = targetAngle*DEG_TO_TICKS;
         isBusy = true;
     }
+    public Vector2 getUnitVectorTarget(Vector3 targetVector){
+        latestDirection.setFromPolar(1, GetTargetAngleRad(targetVector));
+        return latestDirection;
+    }
+    //This function should go in the full system class
+    //public Vector3 getVectorTarget(Vector3 targetVector, double extensionLength){return getUnitVectorTarget(targetVector).toVector3(0).add(0,0,extensionHeight).scalar(extensionLength + retractedLength);}
+    
+    public double GetTargetAngleRad(Vector3 targetVector){
+        return Math.atan2(targetVector.y,targetVector.x,0);
+    }
+    public double GetTargetAngleDeg(Vector3 targetVector){ return Math.toDegrees(GetTargetAngleRad(targetVector));}
+    public void GetTargetPosition(double targetAngle){
+    
     public double GetPos() {return GetRawPos() / TICKS_TO_DEG;}
     public double GetRawPos() {return motor.getCurrentPosition();}
     public double GetTargetPos() {return targetPosition / TICKS_TO_DEG;}
