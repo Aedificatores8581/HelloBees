50f5d26a8388de8469555c710412611baf8d4dff
tacklebat <32886197+tacklebat@users.noreply.github.com>
2025-12-31 08:02:27 -0500

    updated shoulder got it mostly working for height

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susbsystems/Arm725.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susbsystems/Arm725.java
@@ -4,6 +4,7 @@ import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -13,17 +14,18 @@ import org.firstinspires.ftc.teamcode.util.Math.Vector2;
 import org.firstinspires.ftc.teamcode.util.Math.Vector3;
 import org.firstinspires.ftc.teamcode.util.Util;
 
-//TODO 7/23/25 - Frank
-// - set constants
-// - Add error and PID constants to Constants.java
+//TODO 12/30/25 Nathan
+// figure out angle to encoder
+// init function to zero system
+// move to degrees functions maybe needs pid but probably defaults ftc move to position will work well enough
+
 
 public class Arm725 {
+
+    /* not currently used
     public static final double PID_P_DEFAULT = 2.5;
     public static final double PID_I_DEFAULT = 0;
     public static final double PID_D_DEFAULT = .1;
-  
-    private static final double DEG_TO_TICKS = 0.0122222; // copied from teleop, needs re-measured
-
     private static final double HOME_ANGLE = 0;
     private static final double HOME_Height = 0;
 
@@ -36,84 +38,77 @@ public class Arm725 {
     private static final double PIVOT_TO_WRIST = 14;
 
     //private Vector2 latestDirection = new Vector2();
-  
-    private DcMotorEx motor;
-
-    private boolean isBusy = false;
-    private boolean atHome, homed = false, isHoming = false;
-    //value copied from Turret class
-    private double homePower = 0.3;
-    private ElapsedTime homeTime = new ElapsedTime();
-    private double targetPosition;
-    private double currentPosition;
-    private double currentPower = 0;
-    private double pCoef = PID_P_DEFAULT, iCoef = PID_I_DEFAULT, dCoef = PID_D_DEFAULT;
+        private double pCoef = PID_P_DEFAULT, iCoef = PID_I_DEFAULT, dCoef = PID_D_DEFAULT;
     //desired distance offset from the given target in inches
     //this variable can be used to set the extension distance after setting the arm's rotation
 
     private PIDController controller;
 
     public boolean AUTOSTOP = true;
+  */
+    //Shoulder Constants
+    private static final double DEG_TO_TICKS = 11.05;
+    private static final double SHOULDER_LENGTH = 19;
+    private static final double SHOULDER_MAX_HEIGHT = 10;
+    private static final double SHOULDER_MIN_HEIGHT = -10;
+
+    //Shoulder private variables
+    private DcMotorEx motor;
+    private boolean shoulder_is_busy = false;
+    private boolean homed = false;
+    private int targetPosition = 0;
+    private double currentPosition;
+    private double currentPower = 0;
 
     public Arm725(HardwareMap hm) {
         init(hm);
     }
-    public Arm725(HardwareMap hm, double P, double I, double D){
-        init(hm);
-        setPID(P,I,D);
-    }
     private void init(HardwareMap hm) {
         motor = hm.get(DcMotorEx.class, "shouldergobilda");
-        controller = new PIDController(pCoef, iCoef, dCoef);
-        //pot1 = hm.get(AnalogInput.class, "pot1");
-    }
-    public void setPID(double P, double I, double D){
-        pCoef = P; iCoef = I; dCoef = D;
-    }
-    public void StartHome() {
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
         ResetEncoder();
-        homed = true;
-        homeTime.reset();
     }
-    //public void GoTo(Vector3 targetVector) {GoTo(targetVector.z);}
+
     public void GoToHeight(double target_height) {
         
-        this.targetPosition = Math.toDegrees(Math.asin(target_height/PIVOT_TO_WRIST)*DEG_TO_TICKS);
-        isBusy = true;
+        this.targetPosition = (int) Math.toDegrees(Math.asin(target_height)*DEG_TO_TICKS);
+        shoulder_is_busy = true;
+        motor.setTargetPosition((int) targetPosition);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        currentPower = 1;
     }
     public void GoToAngle(double target_angle) {
 
-        this.targetPosition = Math.toDegrees(Math.asin(target_angle/PIVOT_TO_WRIST)*DEG_TO_TICKS);
-        isBusy = true;
-    }
-    //public Vector2 getVectorTarget(Vector3 targetVector){ // No function for GetTargetLength
-    //    latestDirection.setFromPolar(PIVOT_TO_WRIST, getTargetAngleRad(targetVector));
-    //    return latestDirection;
-    //}
-    public double getTargetAngleRad(Vector3 targetVector){
-        return Util.clamp(Math.PI/-2, Math.atan2(targetVector.z,targetVector.toVector2().magnitude()),Math.PI/2);
-    }
-    public double getTargetAngleDeg(Vector3 targetVector){
-        return Math.toDegrees(getTargetAngleRad(targetVector));
+        this.targetPosition = (int) ( target_angle*DEG_TO_TICKS);
+        shoulder_is_busy = true;
+        motor.setTargetPosition((int) targetPosition);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        currentPower = 1;
     }
     public double GetPos() {return currentPosition / DEG_TO_TICKS;}
-    public double GetHeight() {return currentPosition;}
+    public double GetHeight() {
+        double temp;
+        temp = currentPosition / DEG_TO_TICKS;
+        temp = Math.toRadians(temp);
+        temp = Math.sin(temp);
+        return (SHOULDER_LENGTH*temp);
+    }
     public double GetRawPos() {return currentPosition;}
     public double GetTargetPos() {return (targetPosition) / DEG_TO_TICKS;}
     public double GetRawTargetPos() {return targetPosition;}
-    public boolean IsBusy() {return isBusy;}
+    public boolean IsBusy() {return shoulder_is_busy;}
     private void rawSet(double power) {
         motor.setPower(Util.IntClamp(power));
     }
     public void SetPower(double power) {
-        isBusy = false;
+        shoulder_is_busy = false;
         currentPower = power;
     }
     public void Stop() {
         rawSet(0);
         currentPower = 0;
-        isBusy = false;
-        isHoming = false;
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shoulder_is_busy = false;
     }
     public void ResetEncoder() {
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -122,13 +117,34 @@ public class Arm725 {
     public void Update() {
 
         currentPosition = motor.getCurrentPosition();
-        if (isBusy) {
-           controller.setPID(pCoef, iCoef, dCoef);
-           currentPower = controller.calculate(GetPos(), targetPosition);
+        if (shoulder_is_busy) {
+            if (!motor.isBusy()) {
+                shoulder_is_busy = false;
+                motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
         }
-        //add safety checks
+        //safety checks
+        if(homed) {
+            if (GetHeight() > SHOULDER_MAX_HEIGHT) {
+                if (shoulder_is_busy) {
+                    Stop();
+                }
+                currentPower = Math.min(Math.max(currentPower, -1), 0);
+            }
+            if (GetHeight() > SHOULDER_MIN_HEIGHT) {
+                if (shoulder_is_busy) {
+                    Stop();
+                }
+                currentPower = Math.min(Math.max(currentPower, 0), -1);
+            }
+        }
         rawSet(currentPower);
     }
     public double GetPower() {return currentPower;}
     public boolean Homed() {return homed;}
+    public void Home() {
+        homed = true;
+        ResetEncoder();
+    }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susbsystems/tests/shoulderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susbsystems/tests/shoulderTest.java
@@ -5,52 +5,69 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.susbsystems.Arm725;
 import org.firstinspires.ftc.teamcode.util.ButtonBlock;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Shoulder Test", group = "SubsysTest")
 public class shoulderTest extends LinearOpMode{
     Arm725 shoulder;
-    ButtonBlock runToPos, stopArm, dpadUp, dpadDown;
+    ButtonBlock runToPos, stopArm, dpadUp, dpadDown,runToHeight, runToHome;
+    private Servo wrist;
 
     double targetPos = 0;
     @Override
     public void runOpMode() {
         shoulder = new Arm725(hardwareMap);
+        wrist = hardwareMap.get(Servo.class, "wrist");
 
         runToPos = new ButtonBlock().onTrue(() -> {shoulder.GoToAngle(targetPos);});
         stopArm = new ButtonBlock().onTrue(() -> {shoulder.Stop();});
-        dpadUp = new ButtonBlock().onTrue(() -> {targetPos += 2;});
-        dpadDown = new ButtonBlock().onTrue(() -> {targetPos -= 2;});
+        dpadUp = new ButtonBlock().onTrue(() -> {targetPos += 3;});
+        dpadDown = new ButtonBlock().onTrue(() -> {targetPos -= 3;});
+        runToHeight = new ButtonBlock().onTrue(() -> {shoulder.GoToHeight(targetPos);});
+        runToHome = new ButtonBlock().onTrue(() -> {shoulder.Home();});
+
 
         telemetry.addLine("Initialized");
         telemetry.update();
-        //shoulder.StartHome();
-        while (!shoulder.Homed() && !opModeIsActive() && !isStopRequested()) shoulder.Update();
+
+        while (!shoulder.Homed() && !opModeIsActive() && !isStopRequested()) {
+            runToHome.update(gamepad1.x);
+            if (!shoulder.IsBusy()) shoulder.SetPower(gamepad1.right_stick_y);
+            else if (Math.abs(gamepad1.right_stick_y) > 0) {
+                shoulder.Stop();
+                shoulder.SetPower(gamepad1.right_stick_y);
+            }
+            shoulder.Update();
+        }
         waitForStart();
+        wrist.setPosition(1);
         while (opModeIsActive()) {
             runToPos.update(gamepad1.a);
+            runToHeight.update(gamepad1.y);
             stopArm.update(gamepad1.b);
-            if (gamepad1.x) {shoulder.StartHome();}
+            runToHome.update(gamepad1.x);
             dpadUp.update(gamepad1.dpad_up);
             dpadDown.update(gamepad1.dpad_down);
 
-            if (!shoulder.IsBusy()) shoulder.SetPower(gamepad1.right_stick_x);
-            else if (Math.abs(gamepad1.right_stick_x) > 0) {
+            if (!shoulder.IsBusy()) shoulder.SetPower(gamepad1.right_stick_y);
+            else if (Math.abs(gamepad1.right_stick_y) > 0) {
                 shoulder.Stop();
-                shoulder.SetPower(gamepad1.right_stick_x);
+                shoulder.SetPower(gamepad1.right_stick_y);
             }
             shoulder.Update();
             telemetry.addLine("  Controls Guide:");
-            telemetry.addLine("A: Go to Target");
+            telemetry.addLine("A: Go to Target Angle");
             telemetry.addLine("B: Force Stop");
             telemetry.addLine("X: Home");
-            telemetry.addLine("Right Stick X: Rotation");
+            telemetry.addLine("Y: Go to Target Height");
+            telemetry.addLine("Right Stick Y: Up/Down");
             telemetry.addLine();
             telemetry.addLine("  Telemetry Info:");
             telemetry.addData("Motor Power",  shoulder.GetPower());
             telemetry.addData("Homed", shoulder.Homed());
             telemetry.addData("Is Busy", shoulder.IsBusy());
             telemetry.addData("Target Pos", targetPos);
-            telemetry.addData("Current Pos", "Deg: "+shoulder.GetPos()+" Raw: "+shoulder.GetRawPos());
+            telemetry.addData("Current Pos","Deg: "+shoulder.GetPos()+" Raw: "+shoulder.GetRawPos()+" Height: "+shoulder.GetHeight());
             telemetry.addData("Active Target Pos", "Deg: "+shoulder.GetTargetPos()+" Raw: "+shoulder.GetRawTargetPos());
             telemetry.update();
         }
